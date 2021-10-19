8646a63f6e88620f20b2408fb68619684456a71f
XavierLasierra <xlp1998@gmail.com>
2021-10-19 13:17:16 +0200

    Configure Stack Navigator in App.js
    -Install packages react-navigation/native, react-navigation/native-stack, react-native-screens and
    react-native-safe-area-context.
    -Add NavigatorContainer in App and Stack Navigator with a screen Home.

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import Home from "./src/screens/home/Home";
 
 export default function App() {
-  return <Home></Home>;
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
