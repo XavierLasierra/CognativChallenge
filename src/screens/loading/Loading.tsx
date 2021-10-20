ac5650d2c91a27ed356c1b458d976eecdb9baaec
XavierLasierra <xlp1998@gmail.com>
2021-10-20 17:43:16 +0200

    Create Loading component and add it to Suspense

---
+++ src/screens/loading/Loading.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ActivityIndicator, SafeAreaView, View } from "react-native";
+
+import { AppStyles, Colors } from "../../theme";
+import styles from "./loading.styles";
+
+export default function Loading() {
+  return (
+    <SafeAreaView style={AppStyles.screen.mainScreen}>
+      <View style={styles.indicatorContainer}>
+        <ActivityIndicator
+          size="large"
+          color={Colors.black}></ActivityIndicator>
+      </View>
+    </SafeAreaView>
+  );
+}
