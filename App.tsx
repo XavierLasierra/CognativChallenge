095bf2187acae1f5bf7bed8063b350005c2337dc
XavierLasierra <xlp1998@gmail.com>
2021-10-20 12:20:14 +0200

    Install recoil and add RecoilRoot in App

--- App.tsx
+++ App.tsx
@@ -4,15 +4,18 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./src/screens/home/Home";
 import Details from "./src/screens/recipes/Details";
+import { RecoilRoot } from "recoil";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home}></Stack.Screen>
-        <Stack.Screen name="Details" component={Details}></Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RecoilRoot>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home}></Stack.Screen>
+          <Stack.Screen name="Details" component={Details}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
   );
 }
