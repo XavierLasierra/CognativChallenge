6fbf16f16a32e9e348b447c24578cda9e72af79c
XavierLasierra <xlp1998@gmail.com>
2021-10-20 14:05:37 +0200

    Modified recipes service to always return an array

--- src/services/recipes.services.ts
+++ src/services/recipes.services.ts
@@ -3,12 +3,12 @@ import { API_BASE_URL } from "@env";
 
 import { IRecipe } from "../types/interfaces";
 
-export async function getRecipes(): Promise<IRecipe[] | undefined> {
+export async function getRecipes(): Promise<IRecipe[]> {
   try {
-    const { data } = await axios.get(`${API_BASE_URL}/recipes`);
+    const { data }: any = await axios.get(`${API_BASE_URL}/recipes`);
     const { data: recipes } = data;
-    return recipes;
+    return recipes || [];
   } catch (error) {
-    //TODO
+    return [];
   }
 }
