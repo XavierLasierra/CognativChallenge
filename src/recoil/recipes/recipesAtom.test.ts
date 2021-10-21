a6c67f8b6e175d03a34b95cc2031463233bbb958
XavierLasierra <xlp1998@gmail.com>
2021-10-21 17:26:11 +0200

    Add recipe atom and recipeById selectorFamily tests

---
+++ src/recoil/recipes/recipesAtom.test.ts
@@ -0,0 +1,24 @@
+import { snapshot_UNSTABLE } from "recoil";
+import recipesMock from "../../__mocks__/recipes.mock";
+import recipesState, { recipeById } from "./index";
+
+jest.mock("../../services/recipes.services");
+
+describe("Given a recipeById selector", () => {
+  describe("When it is triggered", () => {
+    describe("And recipes atom contains a recipe with the id passed by arguments", () => {
+      test("Then recipeById value should be a recipe with that id", () => {
+        const initialSnapshot = snapshot_UNSTABLE(({ set }) =>
+          set(recipesState, recipesMock),
+        );
+        const [, { _id }] = recipesMock;
+
+        const recibeByIdValue = initialSnapshot
+          .getLoadable(recipeById(_id))
+          .valueOrThrow();
+
+        expect(recibeByIdValue?._id).toBe(_id);
+      });
+    });
+  });
+});
