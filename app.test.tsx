e3039bbcf9a255d490d8413e03c45fd82358b954
XavierLasierra <xlp1998@gmail.com>
2021-10-21 19:16:57 +0200

    Add App component tests

---
+++ app.test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import App from "./App";
+import { render } from "@testing-library/react-native";
+
+import {
+  getRecipes,
+  getRecommendedRecipes,
+} from "./src/services/recipes.services";
+import recipesMock from "./src/__mocks__/recipes.mock";
+
+jest.mock("./src/services/recipes.services");
+jest.useFakeTimers();
+
+describe("Given an App component", () => {
+  describe("When it is rendered", () => {
+    describe("And recipes data is resolved", () => {
+      test("Then should render a Text with Recipes", () => {
+        (getRecipes as jest.Mock).mockReturnValue(recipesMock);
+        (getRecommendedRecipes as jest.Mock).mockReturnValue(
+          recipesMock.slice(0, 2),
+        );
+        const screen = render(<App />);
+
+        const recipeTitle = screen.getByText("Recipes");
+
+        expect(recipeTitle).not.toBe(null);
+      });
+    });
+  });
+});
