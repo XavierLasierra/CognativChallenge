776ed81d2904add9e0f4c65d7c6d9f2cb62f4b84
XavierLasierra <xlp1998@gmail.com>
2021-10-21 19:30:52 +0200

    Create getRecipies service tests

---
+++ src/services/recipesServices.test.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import recipesMock from "../__mocks__/recipes.mock";
+
+import { getRecipes } from "./recipes.services";
+
+jest.mock("axios");
+
+describe("Given a getRecipes function", () => {
+  describe("When it is triggered", () => {
+    describe("And axios.get resolves with recipes data", () => {
+      test("Then should return the recipes it resolves with", async () => {
+        const resolvedData = {
+          data: {
+            data: recipesMock,
+          },
+        };
+        (axios.get as jest.Mock).mockResolvedValue(resolvedData);
+
+        const result = await getRecipes();
+
+        expect(result).toEqual(recipesMock);
+      });
+    });
+
+    describe("And axios.get rejectes", () => {
+      test("Then should return an empty array", async () => {
+        (axios.get as jest.Mock).mockRejectedValue(new Error());
+
+        const result = await getRecipes();
+
+        expect(result).toEqual([]);
+      });
+    });
+  });
+});
